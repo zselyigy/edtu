f9b59e12d4eb41c0bc218d4804f0cb1b8694d727
István Gy. Zsély <zselyigy@gmail.com>
2023-07-18 17:31:23 +0200

    % progrss introduced in random E calculation + minor correction

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def main():
 # constans fixed in the code
     my_filename_c = 'simulationResults_Shrestha2019'   # file name of the simulation results
     my_filename_s = 'sigmas'                           # file name of the estimated sigma values
-    stratas = 200                                     # number of stratas in the sampling
+    stratas = 2000                                     # number of stratas in the sampling
     error_type = 'By data series and datasets'  # 'By datasets (XMLs)','By data series (profiles)','By data series and datasets'
 
 # reading the estimated experimental sigmas file
@@ -123,9 +123,9 @@ def main():
 
     # generation of the random samples
     print('Random sample generation started.')
-    progress_value = numpy.empty(100,int)
-    progress_index = 0
     no_print_steps = 20
+    progress_value = numpy.empty(no_print_steps,int)
+    progress_index = 0
     for i in range(no_print_steps):
         progress_value[i] = numpy.trunc((i + 1) * no_datapoints * stratas / no_print_steps)
         
@@ -142,20 +142,22 @@ def main():
 
 
     # calculation of the overall E value for the nominal values
+    print('Nominal E value calculation started.')
     Etotal = 0
     Exml = 0
     Edataseries = 0
     l = -1
     E_file = open('.\calculatedEvaluesbyPoints.txt','w')
     S_file = open('.\sigmas_used.txt','w')
+
     for i in range(len(xmls)):
         for j in range(len(xmls[i].mydataseries)):
             for k in range(len(xmls[i].mydataseries[j].mydatapoints)):
-                l = l + 1
                 E = ((xmls[i].mydataseries[j].mydatapoints[k][3] - xmls[i].mydataseries[j].mydatapoints[k][2]) / xmls[i].mydataseries[j].mydatapoints[k][1])**2
                 E_file.write(xmls[i].xmlname + ' ' + xmls[i].myspecieslist[j] + ' ' + str(xmls[i].mydataseries[j].mydatapoints[k][0]) + ' ' + str(E) + '\n')
                 S_file.write(xmls[i].xmlname + ' ' + xmls[i].myspecieslist[j] + ' ' + str(xmls[i].mydataseries[j].mydatapoints[k][0]) + ' ' + str(xmls[i].mydataseries[j].mydatapoints[k][1]) + '\n')
                 Edataseries = Edataseries + E
+                l = l + 1
             Edataseries = Edataseries / len(xmls[i].mydataseries[j].mydatapoints)
             Exml = Exml + Edataseries
             Edataseries = 0
@@ -163,12 +165,19 @@ def main():
         Etotal = Etotal + Exml
         Exml = 0
     Etotal = Etotal / len(xmls)
+    print('Nominal E value calculation finished.')
     E_file.close()
     S_file.close()
     print('Nominal Etotal =', Etotal)
 
     # calculation of the overall E value for each random sample
+    print('Random sampled E value calculation started.')
     E_file = open('.\calculatedEvalues.txt','w')
+
+    progress_index = 0
+    for i in range(no_print_steps):
+        progress_value[i] = numpy.trunc((i + 1) * no_datapoints / no_print_steps)
+   
     for s in range(stratas):
         Etotal = 0
         Exml = 0
@@ -177,10 +186,13 @@ def main():
         for i in range(len(xmls)):
             for j in range(len(xmls[i].mydataseries)):
                 for k in range(len(xmls[i].mydataseries[j].mydatapoints)):
-                    l = l + 1
                     E = ((xmls[i].mydataseries[j].mydatapoints[k][4+random_numbers[s][l]] - xmls[i].mydataseries[j].mydatapoints[k][2]) / xmls[i].mydataseries[j].mydatapoints[k][1])**2
 #                    E_file.write(xmls[i].xmlname + ' ' + xmls[i].myspecieslist[j] + ' ' + str(xmls[i].mydataseries[j].mydatapoints[k][0]) + ' ' + str(E) + '\n')
                     Edataseries = Edataseries + E
+                    l = l + 1
+                    if progress_value[progress_index] == l:
+                        progress_index = progress_index + 1
+                        print(str(int(progress_index * 100 / no_print_steps))+'%')
                 Edataseries = Edataseries / len(xmls[i].mydataseries[j].mydatapoints)
                 Exml = Exml + Edataseries
                 Edataseries = 0                
@@ -191,6 +203,8 @@ def main():
 #        print(Etotal)
         E_file.write(str(Etotal)+'\n')
     E_file.close()
+    print('Random sampled E value calculation finished. Data writing to file may take some more time.')
+
                 
 
 if __name__ == "__main__":
